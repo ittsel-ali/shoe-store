653d0949ad3fa637d2ae2bbe9f6b987d94f555cf
Ittsel Ali <ittselali@ittsels-mbp.home>
2024-08-28 08:36:55 -0400

    Modified shoe store to use Kafka

--- inventory.rb
+++ inventory.rb
@@ -1,8 +1,9 @@
-#!/usr/bin/ruby
+require 'kafka'
 
-require 'json'
+kafka = Kafka.new(["localhost:9092"])
 
-STDOUT.sync = true
+producer = kafka.async_producer
+
 
 STORE_STORES = ['ALDO Centre Eaton', 'ALDO Destiny USA Mall', 'ALDO Pheasant Lane Mall', 'ALDO Holyoke Mall', 'ALDO Maine Mall', 'ALDO Crossgates Mall', 'ALDO Burlington Mall', 'ALDO Solomon Pond Mall', 'ALDO Auburn Mall', 'ALDO Waterloo Premium Outlets']
 SHOES_MODELS = ['ADERI', 'MIRIRA', 'CAELAN', 'BUTAUD', 'SCHOOLER', 'SODANO', 'MCTYRE', 'CADAUDIA', 'RASIEN', 'WUMA', 'GRELIDIEN', 'CADEVEN', 'SEVIDE', 'ELOILLAN', 'BEODA', 'VENDOGNUS', 'ABOEN', 'ALALIWEN', 'GREG', 'BOZZA' ]
@@ -10,12 +11,15 @@ INVENTORY = Array(0..100)
 RANDOMNESS = Array(1..3)
 
 loop do
-  RANDOMNESS.sample.times do
-    puts JSON.generate({
-      store: STORE_STORES.sample,
-      model: SHOES_MODELS.sample,
-      inventory: INVENTORY.sample,
-    }, quirks_mode: true)
-  end
+  message = {
+    id: (Time.now.to_f * 1_000_000).to_i,
+    store: STORE_STORES.sample,
+    model: SHOES_MODELS.sample,
+    inventory: INVENTORY.sample,
+  }
+
+  producer.produce(message.to_json, topic: "inventory_updates")
+  producer.deliver_messages
+
   sleep 1
 end
